8a0dd2f8a171765551982914b62b5d00b8367f10
rokosz <46833498+rokosz@users.noreply.github.com>
2019-01-19 00:46:45 -0500

    Create purge.js
    Simple role locked purge.

---
+++ commands/purge.js
@@ -0,0 +1,35 @@
+exports.run = (client, message) => {
+
+  // variables
+  const config = require("./config.json");
+  const args = message.content.split(" ").slice(1);
+
+  // error message
+  const embed = {
+  "title": "Command usage.",
+  "description": "Insufficient data: `\nusage: .purge {number of messages}`",
+  "color": 65423
+  };
+
+  if (message.content.startsWith(config.prefix + "purge")) {
+    if(!message.member.roles.has(devRoleID)) return; // role check [developer only ATM]
+
+    async function purge() {
+      message.delete();
+
+      // if command does not recieve required fields.
+      if (isNaN(args[0])) {
+        message.channel.send({embed});
+        return;
+      }
+      // fetch messages
+      const wiadomosc = await message.channel.fetchMessages({limit: args[0]});
+
+      // deleted the messages once fetched
+      message.channel.bulkDelete(wiadomosc);
+        .catch(error => message.channel.send('$(error)'));
+    }
+    purge();
+  }
+
+}
