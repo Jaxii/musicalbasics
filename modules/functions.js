ae93df7b3504d94a4c9cd283bc022a40da3a9b98
Jaxii <jaxmontelongo@gmail.com>
2019-01-18 22:44:07 -0600

    permission check

--- modules/functions.js
+++ modules/functions.js
@@ -63,3 +63,21 @@ client.loadCommand = (commandName) => {
    }
    return false;
 };
+//basic permission level check
+client.permlevel = message => {
+    let permlvl = 0;
+
+    const permOrder = client.config.permLevels.slice(0).sort((p, c) => p.level < c.level ? 1 : -1);
+
+    while (permOrder.length) {
+      const currentLevel = permOrder.shift();
+      if (message.guild && currentLevel.guildOnly) continue;
+      if (currentLevel.check(message)) {
+        permlvl = currentLevel.level;
+        break;
+      }
+    }
+    return permlvl;
+  };
+
+/*
