db5f30db5b78e4f407f2aca99461172261e0f435
Jaxii <jaxmontelongo@gmail.com>
2019-01-18 20:52:47 -0600

    command handling

--- modules/functions.js
+++ modules/functions.js
@@ -24,3 +24,43 @@ client.clean = async (client, text) => {
 
   return text;
 };
+//command handling
+client.loadCommand = (commandName) => {
+   try {
+     client.logger.log(`Loading Command: ${commandName}`);
+     const props = require(`../commands/${commandName}`);
+     if (props.init) {
+       props.init(client);
+     }
+     client.commands.set(props.help.name, props);
+     props.conf.aliases.forEach(alias => {
+       client.aliases.set(alias, props.help.name);
+     });
+     return false;
+   } catch (e) {
+     return `Unable to load command ${commandName}: ${e}`;
+   }
+ };
+
+ client.unloadCommand = async (commandName) => {
+   let command;
+   if (client.commands.has(commandName)) {
+     command = client.commands.get(commandName);
+   } else if (client.aliases.has(commandName)) {
+     command = client.commands.get(client.aliases.get(commandName));
+   }
+   if (!command) return `The command \`${commandName}\` doesn"t seem to exist, nor is it an alias. Try again!`;
+
+   if (command.shutdown) {
+     await command.shutdown(client);
+   }
+   const mod = require.cache[require.resolve(`../commands/${commandName}`)];
+   delete require.cache[require.resolve(`../commands/${commandName}.js`)];
+   for (let i = 0; i < mod.parent.children.length; i++) {
+     if (mod.parent.children[i] === mod) {
+       mod.parent.children.splice(i, 1);
+       break;
+     }
+   }
+   return false;
+};
